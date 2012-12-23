604d154e52f241553369cbb9917e689924bc44c6
Rasmus Eneman <pie.or.paj@gmail.com>
2012-12-23 11:10:17 +0100

    Fix Devices, Scenarios and Rooms in mobile

--- mobile/urls.py
+++ mobile/urls.py
@@ -7,9 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns('mobile.views',
     url(r'^$', 'index'),
     url(r'^devices/$', 'devices'),
+    url(r'^devices/(?P<room>\d+)/$', 'devices'),
+    url(r'^device/(?P<device>\d+)/cmd/(?P<action>[a-zA-Z0-9_]+)/$', 'device'),
+    url(r'^device/(?P<device>\d+)/$', 'edit_device'),
     url(r'^scenarios/$', 'scenarios'),
+    url(r'^scenario/(?P<scenario>\d+)/$', 'scenario'),
     url(r'^rooms/$', 'rooms'),
     url(r'^settings/$', 'settings'),
-    url(r'^device/(?P<device_id>\d+)/cmd/(?P<action>[a-zA-Z0-9_]+)/$', 'device'),
-    url(r'^device/(?P<device_id>\d+)/$', 'edit_device'),
 )
--- mobile/views.py
+++ mobile/views.py
@@ -3,35 +3,39 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from backend.models.Device import Device
 from backend.models.Scenario import Scenario
+from backend.models.Room import Room
 
 def index(request):
     return render_to_response('mobile/index.html')
 
-def devices(request):
-    device_list = Device.objects.all()
+def devices(request, room=False):
+    if not room:
+        device_list = Device.objects.all()
+    else:
+        device_list = Device.objects.filter(room=room)
     return render_to_response('mobile/devices.html', {'device_list': device_list})
 
-def device(request, device_id, action):
-    device = Device.objects.get(pk=device_id)
+def device(request, device, action):
+    device = Device.objects.get(pk=device)
     device.object.action(action=action)
     return HttpResponseRedirect(reverse('mobile.views.devices'))
 
-def edit_device(request, device_id):
-    p = get_object_or_404(Device, pk=device_id)
+def edit_device(request, device):
+    p = get_object_or_404(Device, pk=device)
     return render_to_response('mobile/device.html', {'device': p})
 
 def scenarios(request):
-    scenario_list = Device.objects.all()
+    scenario_list = Scenario.objects.all()
     return render_to_response('mobile/scenarios.html', {'scenario_list': scenario_list})
 
-def scenario(request, scenario_id):
-    scenario = Scenario.objects.get(pk=scenario_id)
+def scenario(request, scenario):
+    scenario = Scenario.objects.get(pk=scenario)
     scenario.execute()
     return HttpResponseRedirect(reverse('mobile.views.scenarios'))
 
 def rooms(request):
-    room_list = Device.objects.all()
-    return render_to_response('mobile/devices.html', {'room_list': room_list})
+    room_list = Room.objects.all()
+    return render_to_response('mobile/rooms.html', {'room_list': room_list})
 
 def settings(request):
     return render_to_response('mobile/settings.html')
