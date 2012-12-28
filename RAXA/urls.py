f200d36962b60211b6fd1f1f4a13ab5de7cf6b93
Rasmus Eneman <pie.or.paj@gmail.com>
2012-12-28 13:24:07 +0100

    Delete "testapp"

--- RAXA/urls.py
+++ RAXA/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^mobile/', include('mobile.urls')),
+    url(r'^backend/', include('backend.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
