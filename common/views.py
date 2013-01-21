4e30a58386afb5b34bd83c8115c55e5d09b8f631
Rasmus Eneman <pie.or.paj@gmail.com>
2013-01-21 17:06:19 +0100

    Improve performance by prefetching where needed

--- common/views.py
+++ common/views.py
@@ -11,7 +11,7 @@ def overlay(request, floor=1):
         if 'edit_rooms' in request.POST:
             edit_rooms = True
 
-    rooms = Plan.objects.filter(floor=floor)
-    furnitures = Furniture.objects.filter(floor=floor)
+    rooms = Plan.objects.select_related('room__id').filter(floor=floor)
+    furnitures = Furniture.objects.select_related('device').filter(floor=floor)
     radious=10
     return render(request, 'common/floor.svg', {'rooms':rooms, 'furnitures':furnitures, 'radious':radious, 'edit_rooms':edit_rooms})
